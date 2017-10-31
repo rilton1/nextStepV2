edf2c0f014be22e04f48d3c7d5ddacddfabe0a43
Tabata <tdeleite@br.ibm.com>
2017-10-31 20:31:44 +0000

    aa

--- functional/scripts/sampleTest.js
+++ functional/scripts/sampleTest.js
@@ -5,8 +5,6 @@ module.exports = {
       .waitForElementVisible('#mainNav', 60000, true, function(){
       	console.log('found navBar')
       }, 'Waiting for Search Input to become visible')
-      .setValue('input.gsfi', 'Test')
-      .waitForElementPresent('#search-page', 60000)
       .pause(5000)
       .end();
   }
