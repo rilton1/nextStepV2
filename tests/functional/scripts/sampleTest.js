f19cfe659c88e9bf261e6d9744c4e27ec1e23963
Tabata <tdeleite@br.ibm.com>
2017-11-01 12:40:31 +0000

    test

--- tests/functional/scripts/sampleTest.js
+++ tests/functional/scripts/sampleTest.js
@@ -1,11 +1,12 @@
 module.exports = {
   'Sample functional test' : function (browser) {
     browser
-      .url('http://nextstep-test.mybluemix.net/')
-      .pause(3000)
-      .waitForElementVisible('#skills', 60000)
-      // .setValue('input.gsfi', 'Test')
-      // .waitForElementPresent('#search-pag', 60000)
+      .url('http://google.com')
+      .waitForElementVisible('input.gsfi', 60000, true,
+        function(){}, 'Waiting for Search Input to become visible')
+      .setValue('input.gsfi', 'Test')
+      .waitForElementPresent('#search-page', 60000)
+      .pause(5000)
       .end();
   }
 };
